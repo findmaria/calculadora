b4b8a2cb673b451ec071007d9c1e70bba31adcd5
Camy <101035211+findmaria@users.noreply.github.com>
2024-07-31 15:01:47 -0300

    refiz a calculadora usando tabela

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
 function Calculadora(){
     this.display = document.querySelector('.display');
 
-    this.capturaCliques = () => {
-        document.addEventListener('click', e => {
-            const el = e.target;
-            if (el.classList.contains('btn-num'))this.addNumDisplay(el);
-            if (el.classList.contains('op'))this.addNumDisplay(el);
-            if (el.classList.contains('clear'))this.clear();
-            if (el.classList.contains('del'))this.del(el);
-            if (el.classList.contains('equal'))this.realizaConta(el);
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    };
+
+    this.capturaEnter = () => {
+        document.addEventListener('keyup', e => {
+            if (e.key !== 'Enter') return;
+            this.realizaConta();
         });
     };
 
-    //el.innerText adiciona o texto interno do botão na tela (display)
+    this.capturaCliques = () => {
+        document.addEventListener('click', e => {
+            const el = e.target;
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if (el.classList.contains('btn-clear')) this.clear();
+            if (el.classList.contains('btn-del')) this.del();
+            if (el.classList.contains('btn-eq')) this.realizaConta();
+        });
+    };
 
-    this.addNumDisplay = el => this.display.value += el.innerText;
+    this.addNumDisplay = el => {
+        this.display.value +=  el.innerText;
+        this.display.focus();
+    };
+
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+
+    this.realizaConta = () => {
+        try{
+            const conta = eval(this.display.value);
+
+            if(!conta){
+                alert('Conta inválida');
+                return;
+            }
+
+            this.display.value = conta;
+
+        }catch(e){
+            alert('Conta inválida!');
+            return;
+        }
+    }
+
     this.clear = () => this.display.value = '';
-    this.inicia = () => this.capturaCliques();
+};
 
-    this.del = () =>
-        this.display.value = this.display.value.slice(0, -1);
-}
 const calculadora = new Calculadora();
 calculadora.inicia();
